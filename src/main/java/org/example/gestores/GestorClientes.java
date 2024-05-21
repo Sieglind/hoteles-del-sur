b39e687d10c30b12b5a2eaa1ea4fb8089e2b9255
Sieglind <moraledacd@gmail.com>
2024-05-21 20:54:21 -0300

    Fixed missing import

--- src/main/java/org/example/gestores/GestorClientes.java
+++ src/main/java/org/example/gestores/GestorClientes.java
@@ -1,6 +1,9 @@
 package org.example.gestores;
 
+import org.example.usuarios.Cliente;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class GestorClientes {
     private final List<Cliente> listaClientes;
