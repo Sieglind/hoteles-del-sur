0883c2eea45bc1afb428787ca2b41614cfe789de
Sieglind <moraledacd@gmail.com>
2024-06-11 10:39:39 -0300

    Implementado IGestor en GestorReservas

--- src/main/java/org/example/entidades/Habitacion.java
+++ src/main/java/org/example/entidades/Habitacion.java
@@ -14,4 +14,39 @@ public class Habitacion {
         this.tipoDeHabitacion = tipoDeHabitacion;
         this.disponible = true;
     }
+
+    public String getNumeroDeHabitacion() {
+        return numeroDeHabitacion;
+    }
+
+    public TipoDeHabitacion getTipoDeHabitacion() {
+        return tipoDeHabitacion;
+    }
+
+    public void setTipoDeHabitacion(TipoDeHabitacion tipoDeHabitacion) {
+        this.tipoDeHabitacion = tipoDeHabitacion;
+    }
+
+    public Integer getNumeroDeCamas() {
+        return numeroDeCamas;
+    }
+
+    public void setNumeroDeCamas(Integer numeroDeCamas) {
+        this.numeroDeCamas = numeroDeCamas;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Habitacion habitacion) {
+            return this.numeroDeHabitacion.equals(habitacion.numeroDeHabitacion);
+        } else return false;
+    }
 }
