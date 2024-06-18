36a2cc8aa28bbb478a407bb5b5b315d6aa3e4aa4
cristian.moraleda <moraledcd@gmail.com>
2024-06-18 09:53:31 -0300

    Nuevos Cambios a menu

--- src/main/java/org/example/menues/acciones/AccionCuadroTareas.java
+++ src/main/java/org/example/menues/acciones/AccionCuadroTareas.java
@@ -1,4 +1,6 @@
 package org.example.menues.acciones;
+import org.example.menues.VentanaPrincipal;
+import org.example.menues.cuadros.tareas.Renderizable;
 import org.example.menues.enums.Entidad;
 import org.example.menues.enums.Tarea;
 
@@ -17,12 +19,18 @@ public class AccionCuadroTareas implements ActionListener {
         Tarea tarea = Tarea.valueOf(button.getText());
         try {
             Class<?> claseDeEntidad = Class.forName(obtenerNombreDeEntidad(entidad));
-            //TODO Un solo panel con multiples formularios y mostramos solo el que queremos usar?
-            //  o multiples paneles uno para cada cosa que se puede hacer?
+            //VentanaPrincipal.cambiarCuadroEnVentanaPrincipal(seleccionarPanel(tarea,claseDeEntidad));
         } catch (ClassNotFoundException exception) {
             System.out.println("No se encontro el tipo de entidad");
         }
     }
+
+//    private JPanel seleccionarPanel(Tarea tarea, Class<?> entidad) {
+//        switch (tarea) {
+//            case CREAR -> {
+//            }
+//        }
+//    }
 
     private String obtenerNombreDeEntidad(Entidad entidad) {
         return switch (entidad) {
---
+++ src/main/java/org/example/menues/cuadros/tareas/impl/TareasCliente.java
@@ -0,0 +1,7 @@
+package org.example.menues.cuadros.tareas.impl;
+
+import org.example.menues.cuadros.JPanelCustom;
+
+public class TareasCliente<E> extends JPanelCustom {
+
+}
