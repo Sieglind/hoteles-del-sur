48d0938167a08ef2183a9af4105dc944510ffbc5
whtshdowmdq <facucuadradomdq@gmail.com>
2024-06-21 11:41:41 -0300

    Cambios en los paneles de habitacion

---
+++ src/main/java/org/example/menues/cuadros/cuadroscaja/PanelHabitacion.java
@@ -0,0 +1,61 @@
+package org.example.menues.cuadros.cuadroscaja;
+
+import org.example.sistema.entidades.Habitacion;
+import org.example.sistema.enums.TipoDeHabitacion;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+
+public class PanelHabitacion extends CuadroCajaCustom {
+    private final JLabel ETIQUETA_NUMERO_HABITACION = crearEtiqueta("Numero de Habitacion: ");
+    private final JTextField CAMPO_NUMERO_HABITACION = crearCampoDeTexto();
+    private final JLabel ETIQUETA_TIPO_HABITACION = crearEtiqueta("Tipo de Habitacion: ");
+    private final JComboBox<TipoDeHabitacion> CAMPO_TIPO_HABITACION = new JComboBox<>(TipoDeHabitacion.values());
+    private final JCheckBox DISPONIBILIDAD = new JCheckBox("Disponible");
+
+    public PanelHabitacion(boolean editable) {
+        this.setVisible(editable);
+        dimensionarCompomente(CAMPO_TIPO_HABITACION, CENTER_ALIGNMENT);
+        this.add(ETIQUETA_NUMERO_HABITACION);
+        this.add(CAMPO_NUMERO_HABITACION);
+        this.add(ETIQUETA_TIPO_HABITACION);
+        this.add(CAMPO_TIPO_HABITACION);
+        this.add(DISPONIBILIDAD);
+        if (!editable) {
+            this.CAMPO_NUMERO_HABITACION.setEnabled(false);
+            this.CAMPO_NUMERO_HABITACION.setDisabledTextColor(Color.BLACK);
+            this.CAMPO_TIPO_HABITACION.setEnabled(false);
+            this.DISPONIBILIDAD.setEnabled(false);
+        }
+        this.add(this.CAMPO_NUMERO_HABITACION);
+        this.add(this.CAMPO_TIPO_HABITACION);
+    }
+
+    @Override
+    protected void paintComponent(Graphics fondo) {
+        this.setBackground(Color.WHITE);
+        this.setOpaque(true);
+    }
+
+    private JLabel crearEtiqueta(String texto) {
+        return crearEtiqueta(texto, Component.LEFT_ALIGNMENT);
+    }
+
+    private JTextField crearCampoDeTexto() {
+        return crearCampoDeTexto(CENTER_ALIGNMENT);
+    }
+
+    public void fillValues(Habitacion habitacion) {
+        Border border = new LineBorder(Color.BLACK, 3);
+        this.setBorder(new TitledBorder(border, "Resultados"));
+        this.CAMPO_NUMERO_HABITACION.setText(habitacion.getNumeroDeHabitacion());
+        this.CAMPO_TIPO_HABITACION.setSelectedItem(habitacion.getTipoDeHabitacion());
+        this.DISPONIBILIDAD.setSelected(habitacion.isDisponible());
+        this.setVisible(true);
+        this.revalidate();
+        this.repaint();
+    }
+}
--- src/main/java/org/example/menues/cuadros/cuadroscaja/tareas/impl/TareasHabitacion.java
+++ src/main/java/org/example/menues/cuadros/cuadroscaja/tareas/impl/TareasHabitacion.java
@@ -1,8 +1,7 @@
 package org.example.menues.cuadros.cuadroscaja.tareas.impl;
 
 import org.example.menues.acciones.AccionVolver;
-import org.example.menues.cuadros.cuadroscaja.CuadroBotonesZocalo;
-import org.example.menues.cuadros.cuadroscaja.CuadroCajaCustom;
+import org.example.menues.cuadros.cuadroscaja.*;
 import org.example.menues.cuadros.cuadroscaja.tareas.Tareas;
 import org.example.menues.enums.Entidad;
 import org.example.menues.enums.Tarea;
@@ -15,21 +14,21 @@ import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.util.List;
+import java.util.Vector;
 
 
 public class TareasHabitacion extends CuadroCajaCustom implements Tareas {
-    private final JLabel ETIQUETA_NUMERO_HABITACION = crearEtiqueta("Numero de Habitacion: ");
-    private final JTextField CAMPO_NUMERO_HABITACION = crearCampoDeTexto();
-    private final JLabel ETIQUETA_TIPO_HABITACION = crearEtiqueta("Tipo de Habitacion: ");
-    private final JComboBox<TipoDeHabitacion> CAMPO_TIPO_HABITACION = new JComboBox<>(TipoDeHabitacion.values());
-    private final JCheckBox DISPONIBILIDAD = new JCheckBox("Disponible");
+
     private final JButton BOTON_VOLVER = crearBoton("Volver", LEFT_ALIGNMENT, new AccionVolver(Entidad.HABITACIONES.name()));
+
+    private PanelDeEntradas panelDeEntradas;
+    private PanelHabitacion panelHabitacion;
+    private PanelBotones panelBotones;
 
     public TareasHabitacion(Tarea tarea) {
         super();
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         this.setVisible(true);
-        dimensionarCompomente(CAMPO_TIPO_HABITACION, LEFT_ALIGNMENT);
         seleccionarPanel(tarea);
     }
 
@@ -47,18 +46,10 @@ public class TareasHabitacion extends CuadroCajaCustom implements Tareas {
     @Override
     public void panelCrear() {
         this.setBorder(new TitledBorder("Crear Habitación"));
-        limpiarCampos();
-        this.add(ETIQUETA_NUMERO_HABITACION);
-        this.add(CAMPO_NUMERO_HABITACION);
-        this.add(ETIQUETA_TIPO_HABITACION);
-        this.add(CAMPO_TIPO_HABITACION);
 
-
-        JPanel zocalo = new CuadroBotonesZocalo();
-        JButton guardar = new JButton("Guardar");
-        zocalo.add(guardar);
-        zocalo.add(BOTON_VOLVER);
-        this.add(zocalo);
+        this.panelDeEntradas = crearPanelDeEntradas(false);
+        this.panelHabitacion = crearPanelHabitacion(true);
+        this.panelBotones = crearPanelBotones(Tarea.CREAR);
 
     }
 
@@ -67,97 +58,67 @@ public class TareasHabitacion extends CuadroCajaCustom implements Tareas {
     public void panelBuscar() {
         this.setBorder(new TitledBorder("Buscar Habitacion"));
 
-        this.add(ETIQUETA_NUMERO_HABITACION);
-        this.add(CAMPO_NUMERO_HABITACION);
+        this.panelDeEntradas = crearPanelDeEntradas(true);
+        this.panelHabitacion = crearPanelHabitacion(true);
+        this.panelBotones = crearPanelBotones(Tarea.CREAR);
 
-        JPanel Zocalo = new CuadroBotonesZocalo();
-        JButton guardar = new JButton("Guardar");
-        Zocalo.add(guardar);
-        Zocalo.add(BOTON_VOLVER);
-        this.add(Zocalo);
     }
 
     @Override
     public void panelListar() {
         this.setBorder(new TitledBorder("Listar Habitaciones"));
         List<Habitacion> habitaciones = Sistema.getInstance().listarHabitaciones();
-
-        JPanel panelDeHabitaciones = new JPanel();
-        panelDeHabitaciones.setLayout(new BoxLayout(panelDeHabitaciones, BoxLayout.Y_AXIS));
-        habitaciones.forEach(habitacion -> {
-            JPanel panelDeHabitacion = new JPanel();
-            panelDeHabitacion.setBorder(new LineBorder(Color.black, 1));
-            JLabel numeroHabitacion = new JLabel("Numero de habitacion: " + habitacion.getNumeroDeHabitacion());
-            JLabel tipoDeHabitacion = new JLabel("Tipo de Habitacion: " + habitacion.getTipoDeHabitacion());
-            panelDeHabitacion.add(numeroHabitacion);
-            panelDeHabitacion.add(tipoDeHabitacion);
-            panelDeHabitacion.setVisible(true);
-            dimensionarCompomente(panelDeHabitacion,LEFT_ALIGNMENT);
-            panelDeHabitaciones.add(panelDeHabitacion);
-        });
-        JScrollPane contenedorDeLista = new JScrollPane(panelDeHabitaciones);
-        this.add(contenedorDeLista);
-
-
-        JPanel zocalo = new CuadroBotonesZocalo();
-        zocalo.add(BOTON_VOLVER);
-        this.add(zocalo);
+        JList<Habitacion> listaHabitaciones = new JList<>(new Vector<>(habitaciones));
+        this.add(new JScrollPane(listaHabitaciones));
+        this.panelBotones = crearPanelBotones(Tarea.LISTAR);
     }
 
     @Override
     public void panelActualizar() {
         this.setBorder(new TitledBorder("Actualizar Habitacion"));
 
-        this.add(ETIQUETA_NUMERO_HABITACION);
-        this.add(CAMPO_NUMERO_HABITACION);
-
-        JPanel zocalo = new CuadroBotonesZocalo();
-        JButton guardar = new JButton("Guardar");
-        zocalo.add(guardar);
-
-        this.add(ETIQUETA_NUMERO_HABITACION);
-        this.add(ETIQUETA_TIPO_HABITACION);
-        this.add(DISPONIBILIDAD);
-        JButton actualizar = new JButton("Actualizar");
-        zocalo.add(actualizar);
-        zocalo.add(BOTON_VOLVER);
-        this.add(zocalo);
-
+        this.panelDeEntradas = crearPanelDeEntradas(true);
+        this.panelBotones = crearPanelBotones(Tarea.ACTUALIZAR);
     }
 
     @Override
     public void panelEliminar() {
         this.setBorder(new TitledBorder("Eliminar Habitacion"));
 
-        this.add(ETIQUETA_NUMERO_HABITACION);
-        this.add(CAMPO_NUMERO_HABITACION);
-
-        JPanel zocalo = new CuadroBotonesZocalo();
-        JButton buscar = new JButton("Buscar");
-        zocalo.add(buscar);
-        JButton eliminar = new JButton("Eliminar");
-        zocalo.add(eliminar);
-        zocalo.add(BOTON_VOLVER);
-        this.add(zocalo);
+        this.panelDeEntradas = crearPanelDeEntradas(true);
+        this.panelBotones = crearPanelBotones(Tarea.BORRAR);
 
     }
 
-    private void limpiarCampos() {
-        CAMPO_NUMERO_HABITACION.setText("");
-        CAMPO_TIPO_HABITACION.setSelectedItem(null);
-        DISPONIBILIDAD.setSelected(false);
-    }
+   private PanelDeEntradas crearPanelDeEntradas(boolean editable){
+        PanelDeEntradas panelDeEntrada = new PanelDeEntradas(editable);
+        this.add(panelDeEntrada,crearConfiguracion(0.1,0));
+        return panelDeEntrada;
+   }
 
-    public Habitacion obtenerNuevaHabitacion() {
-        return new Habitacion(CAMPO_NUMERO_HABITACION.getText(), TipoDeHabitacion.valueOf(CAMPO_TIPO_HABITACION.getSelectedItem().toString()));
-    }
+   private PanelHabitacion crearPanelHabitacion(boolean editable){
+        PanelHabitacion panelHabitacion = new PanelHabitacion(editable);
+        return crearPanelHabitacion(panelHabitacion);
+   }
 
-    private JLabel crearEtiqueta(String texto) {
-        return crearEtiqueta(texto, Component.LEFT_ALIGNMENT);
-    }
+   private PanelHabitacion crearPanelHabitacion(PanelHabitacion panelHabitacion){
+        this.add(panelHabitacion,crearConfiguracion(0.8,1));
+        return panelHabitacion;
+   }
 
-    private JTextField crearCampoDeTexto() {
-        return crearCampoDeTexto(LEFT_ALIGNMENT);
+   private PanelBotones crearPanelBotones(Tarea tarea){
+        PanelBotones panelBotones = new PanelBotones(tarea,BOTON_VOLVER);
+        this.add(panelBotones,crearConfiguracion(0.1,2));
+        return panelBotones;
+   }
+    private GridBagConstraints crearConfiguracion(double weighty, int posicion) {
+        GridBagConstraints configuracion = new GridBagConstraints();
+        configuracion.weightx = 1.0;
+        configuracion.weighty = weighty;
+        configuracion.gridx = posicion;
+        configuracion.insets = new Insets(30, 30, 30, 30);
+        configuracion.fill = GridBagConstraints.BOTH;
+        return configuracion;
     }
 
 }
