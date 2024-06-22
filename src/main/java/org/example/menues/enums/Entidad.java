ee29fa23e7783548a1586703d9d23d2371ebc009
Sieglind <moraledacd@gmail.com>
2024-06-22 15:47:37 -0300

    Removido atributo innecesario de enum

--- src/main/java/org/example/menues/enums/Entidad.java
+++ src/main/java/org/example/menues/enums/Entidad.java
@@ -1,16 +1,10 @@
 package org.example.menues.enums;
 
 public enum Entidad {
-    CLIENTES("Cliente"),
-    EMPLEADOS("Empleado"),
-    RESERVAS("Reserva"),
-    HABITACIONES("Habitacion"),
-    SERVICIOS("Servicios");
-
-    private final String entidad;
-
-    Entidad(String entidad) {
-        this.entidad = entidad;
-    }
+    CLIENTES(),
+    EMPLEADOS(),
+    RESERVAS(),
+    HABITACIONES(),
+    SERVICIOS();
 
 }
